d535da45fd029815cd54cc64814db27666c9be0c
Andres Ornelas <andres@twitter.com>
2018-09-20 23:00:37 -0700

    Replace Debug Ray with Line Renderer

--- Assets/MagicBubbles/Scripts/EyeTracking.cs
+++ Assets/MagicBubbles/Scripts/EyeTracking.cs
@@ -7,24 +7,28 @@ namespace MagicBubbles.Scripts
     {
         public Transform EyeGaze;
         public TelekinesisController TelekinesisController;
+
         private MeshRenderer _renderer;
+        private LineRenderer _gazeRay;
 
         private const float ConfidenceThreshold = 0.9f;
+        private const float GazeRayWidth = 0.01f;
 
         private void Awake()
         {
             _renderer = EyeGaze.GetComponent<MeshRenderer>();
+            _gazeRay = gameObject.AddComponent<LineRenderer>();
+            _gazeRay.startWidth = GazeRayWidth;
+            _gazeRay.endWidth = GazeRayWidth;
 
             if (!MagicLeapDevice.IsReady()) {
                 Debug.LogWarning("Disabling MagicBubbles.Scripts.EyeTracking because MagicLeapDevice wasn't ready.");
-//                enabled = false;
                 return;
             }
 
             MLEyes.Start();
             if (!MLEyes.IsStarted) {
                 Debug.LogWarning("Disabling MagicBubbles.Scripts.EyeTracking because MLEyes didn't start.");
-//                enabled = false;
             }
         }
 
@@ -35,6 +39,12 @@ namespace MagicBubbles.Scripts
                 return;
             }
             _renderer.enabled = show;
+
+            if (_gazeRay == null) {
+                Debug.LogWarning("Ignoring null _gazeRay");
+                return;
+            }
+            _gazeRay.enabled = show;
         }
 
         private void Update()
@@ -42,12 +52,14 @@ namespace MagicBubbles.Scripts
             if (MLEyes.IsStarted && MLEyes.FixationConfidence > ConfidenceThreshold)
                 EyeGaze.position = MLEyes.FixationPoint;
 
-            var raySource = MLEyes.IsStarted ? MLEyes.LeftEye.Center : Vector3.zero;
+            var raySource = MLEyes.IsStarted
+                ? MLEyes.LeftEye.Center - MLEyes.RightEye.Center
+                : Vector3.zero + new Vector3(0, 0.1f, 0);
             var rayDir = EyeGaze.position - raySource;
-            Debug.DrawRay(raySource, rayDir, Color.green);
+            _gazeRay.SetPositions(new[] { raySource, EyeGaze.position });
 
             RaycastHit hit;
-            if (Physics.Raycast(raySource, rayDir, out hit, 5))
+            if (Physics.Raycast(raySource, rayDir, out hit, 10))
             {
                 if (hit.collider.CompareTag("Bubble")) {
                     TelekinesisController.HoldBubble(hit.collider.gameObject);
